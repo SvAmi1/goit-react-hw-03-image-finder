c55aaac4e8bdfd46d81ad286cfe5260baf872ae6
SvAmi1 <svmoshko877@gmail.com>
2023-08-26 16:53:30 +0300

    npm i

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
-export const App = () => {
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+
+  };
+render () {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
     </div>
   );
+}
 };
